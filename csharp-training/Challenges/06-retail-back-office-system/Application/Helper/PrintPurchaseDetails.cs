50166315d92ccb5a0eb9d0897ea227db7288eed3
Goshik Ahotar <goshik.ahotar@gmail.com>
2026-06-17 22:18:10 +0400

    fix: refactor

--- csharp-training/Challenges/06-retail-back-office-system/Application/Helper/PrintPurchaseDetails.cs
+++ csharp-training/Challenges/06-retail-back-office-system/Application/Helper/PrintPurchaseDetails.cs
@@ -1,6 +1,20 @@
-﻿namespace csharp.Challenges._06_retail_back_office_system.Application.Helper;
+﻿using csharp.Challenges._06_retail_back_office_system.Domain;
+
+namespace csharp.Challenges._06_retail_back_office_system.Application.Helper;
 
 public class PrintPurchaseDetails
 {
-    
+    public void PrintStockOfSelectedItems(int storeId, List<StoreLineRequest> itemsSelectedByCustomer, List<StockItem> storeProductList)
+    {
+        foreach (var item in itemsSelectedByCustomer)
+        {
+            var productId = item.ProductId;
+            var stockItem =
+                storeProductList.FirstOrDefault(x => x.ProductId == productId && x.StoreId == storeId);
+
+            int quantityInStock = stockItem != null ? stockItem.QuantityAvailable : 0;
+            
+            Console.WriteLine("Store : " + storeId + ", Product: " +productId + ", QuantityInStock: " + quantityInStock);
+        }
+    }
 }
